c79a3da4fb35cf66cbe4c654fb73eb7956c5ae5d
Habib Al Faruq <2511532010_habib@student.unand.ac.id>
2025-11-10 11:47:00 +0700

    Add files via upload

---
+++ src/pekan7_2511532010/PanggilMahasiswa_2511532010.java
@@ -0,0 +1,14 @@
+package pekan7_2511532010;
+
+public class PanggilMahasiswa_2511532010 {
+
+	public static void main(String[] args) {
+		Mahasiswa_2511532010 a = new Mahasiswa_2511532010 ();
+		a.setNim(23532);
+		a.setNama("Rahmat");
+		System.out.println (a.getNim ());
+		System.out.println (a.getNama ());
+		a.Cetak();
+	}
+
+}
